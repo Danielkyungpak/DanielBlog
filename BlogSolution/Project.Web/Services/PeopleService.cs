707c256abb695fbf03a4906090f07c92b36eb48e
Daniel <danielkyungpak@gmail.com>
2017-06-13 13:23:06 -0700

    Initialize Blog

---
+++ BlogSolution/Project.Web/Services/PeopleService.cs
@@ -0,0 +1,42 @@
+﻿using Project.Web.Models.View;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Project.Web.Services
+{
+    public class PeopleService
+    {
+        public List<Person> GetPeople()
+        {
+            List<Person> peopleList = new List<Person>();
+
+            string connString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+            using (SqlConnection sqlConn = new SqlConnection(connString))
+
+            {
+                using (SqlCommand cmd = new SqlCommand("People_SelectAll", sqlConn))
+                {
+                    sqlConn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                    while (reader.Read())
+                    {
+                        Person p = new Person();
+                        p.Id = reader.GetInt32(0);
+                        p.FirstName = reader.GetString(1);
+                        p.LastName = reader.GetString(2);
+                        peopleList.Add(p);
+
+                    }
+                }   
+                
+            }
+
+
+                return peopleList;
+        }
+    }
+}
