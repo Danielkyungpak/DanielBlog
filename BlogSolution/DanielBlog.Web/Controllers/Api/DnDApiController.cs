17ae4d9b824a24feb570b701342426cd0cb5204a
Daniel Pak - Desktop <danielkyungpak@gmail.com>
2017-08-31 10:56:21 -0700

    OTG Commit

---
+++ BlogSolution/DanielBlog.Web/Controllers/Api/DnDApiController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DanielBlog.Web.Controllers.Api
+{
+    public class DnDApiController : ApiController
+    {
+        //Routes
+
+
+        //Character Detail GetAllCharacters
+
+        //Character Detail GetById
+
+        //MainStats GetByCharacterId
+
+        //MainStats Update
+
+        //Health GetByCharacterId
+
+        //Health Update
+
+
+        //HitDice GetByCharacterId
+
+        //HitDice Update
+
+        //DeathSaves GetByCharacterId
+
+        //DeathSaves Update
+
+
+        //Skill Specialty GetByCharacterId
+
+        //Skill Specialty Update
+
+
+        //Creation of Character Full Db Call
+
+
+        //Get Full Character Information with Modifiers 
+    }
+}
---
+++ BlogSolution/DanielBlog.Web/Services/DnDService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DanielBlog.Web.Services
+{
+    public class DnDService
+    {
+        //Character Detail Insert
+        //Character Detail GetAllCharacters
+        //Character Detail GetById
+
+        //MainStats Insert
+        //MainStats GetByCharacterId
+        //MainStats Update
+
+        //Health Insert
+        //Health GetByCharacterId
+        //Health Update
+
+        //HitDice Insert
+        //HitDice GetByCharacterId
+        //HitDice Update
+
+        //DeathSaves Insert
+        //DeathSaves GetByCharacterId
+        //DeathSaves Update
+
+        //Skill Value Insert
+        //Skill Value Update
+        //Skill Value GetByCharacterId
+
+        //Skill Specialty Insert
+        //Skill Specialty GetByCharacterId
+        //Skill Specialty Update
+
+
+        //Processing Modifiers (private method)
+
+
+
+
+
+        //Creation of Character Full Db Calls, details,stats,health,hitdice,deathsaves,skillvalues,skillspecialty
+        //Get Full Character Information with Modifiers, details,statvalues,modifiers,health,hitdice,deathsaves,skillvalues
+    }
+}
